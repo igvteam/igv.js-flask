6f6e5051edda49805fc22419463a9aba23b8d738
Jim Robinson <jrobinso@broadinstitute.org>
2017-09-27 12:49:04 -0700

    Enable CORS by default

--- igvjs/_config.py
+++ igvjs/_config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 ALLOWED_EMAILS = "allowed_emails.txt"
 USES_OAUTH = False
 PUBLIC_DIR = "/static/data/public"
-ENABLE_CORS_REQUESTS = False
+ENABLE_CORS_REQUESTS = True
 
 DEBUG = False
